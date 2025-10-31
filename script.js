3b482c64163191b28b4792b06d14e8e7b130fcbd
Alexander Radchenko <alex.radchenko.by@gmail.com>
2025-10-31 12:21:13 +0300

    feat: added buttons and styles

---
+++ script.js
@@ -0,0 +1,48 @@
+const newDiv = document.createElement("div");
+
+newDiv.className = "class-div";
+newDiv.id = "div-id";
+
+document.body.appendChild(newDiv);
+
+const newButtonA = document.createElement('button');
+newButtonA.textContent = 'A';
+newButtonA.className = 'button';
+newButtonA.id = 'button-id-A';
+newDiv.appendChild(newButtonA);
+
+const newButtonS = document.createElement('button');
+newButtonS.textContent = 'S';
+newButtonS.className = 'button';
+newButtonS.id = 'button-id-S';
+newDiv.appendChild(newButtonS);
+
+const newButtonD = document.createElement('button');
+newButtonD.textContent = 'D';
+newButtonD.className = 'button';
+newButtonD.id = 'button-id-D';
+newDiv.appendChild(newButtonD);
+
+const newButtonF = document.createElement('button');
+newButtonF.textContent = 'F';
+newButtonF.className = 'button';
+newButtonF.id = 'button-id-F';
+newDiv.appendChild(newButtonF);
+
+const newButtonG = document.createElement('button');
+newButtonG.textContent = 'G';
+newButtonG.className = 'button';
+newButtonG.id = 'button-id-G';
+newDiv.appendChild(newButtonG);
+
+const newButtonH = document.createElement('button');
+newButtonH.textContent = 'H';
+newButtonH.className = 'button';
+newButtonH.id = 'button-id-H';
+newDiv.appendChild(newButtonH);
+
+const newButtonJ = document.createElement('button');
+newButtonJ.textContent = 'J';
+newButtonJ.className = 'button';
+newButtonJ.id = 'button-id-J';
+newDiv.appendChild(newButtonJ);
